ba1086e3cef2ea467306f26703465bd8706a2937
Никулин Юрий <nikulinyura@MacBook-Pro.local>
2023-04-29 18:07:56 +0500

    Material

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "./shared/Layout";
+import './main.global.css';
+import { Header } from "./shared/Header";
+
+export function App() {
+    return (
+        <Layout>
+            <Header />
+        </Layout>
+    );
+}
